1f01160a01f2f9234d3a0822f48104aa458f333a
shiyu <379771379@qq.com>
2016-05-10 20:30:30 +0800

    Customizing table, column and foreign key column names when using entity framework code first approach

--- EntityFramworkDemos/Employee.cs
+++ EntityFramworkDemos/Employee.cs
@@ -1,13 +1,20 @@
-﻿namespace EntityFramworkDemos
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace EntityFramworkDemos
 {
+    [Table("Employee")]
     public class Employee
     {
         public int Id { get; set; }
+        [Column("Fist_Name")]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
         public int Salary { get; set; }
+        public int DepartmentId { get; set; }
 
+        [ForeignKey("DepartmentId")]
         public Department Department { get; set; }
     }
 }
